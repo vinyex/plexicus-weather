b04aa7b42e3196ec69dbfe1a5cfaf63265883df2
Irvine Pramudya <irvinepramudya@gmail.com>
2024-04-23 06:39:54 +0700

    feat: update active current location table

--- components/LocationTable.vue
+++ components/LocationTable.vue
@@ -18,17 +18,17 @@
 					:key="`location-${index}`"
 					class="border-b border-gray-700"
 					:class="{
-						'bg-green-400 text-white': index === activeCurrent,
-						'bg-gray-800': index !== activeCurrent,
+						'bg-green-400 text-white': item.state === activeCurrent,
+						'bg-gray-800': item.state !== activeCurrent,
 					}"
 				>
 					<th
 						scope="row"
-						class="text-center px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+						class="text-center px-6 py-4 font-medium whitespace-nowrap text-white"
 					>
 						{{ item.name }}
 					</th>
-					<td class="text-center px-6 py-4">{{ item.state }}</td>
+					<td class="text-center text-white px-6 py-4">{{ item.state }}</td>
 					<td class="text-center px-6 py-4">
 						<button
 							class="inline-flex justify-center items-center py-2 px-2 md:py-3 md:px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900"
@@ -54,7 +54,7 @@ defineProps({
 /**
  * Define active location
  */
-const activeCurrent = ref(-1);
+const activeCurrent = ref();
 
 /**
  * composables functions inside useGlobal context
@@ -64,8 +64,8 @@ const { findWeather } = useGlobal();
 /**
  * Finding weather after clicked button
  */
-const findingWeather = async (item: any, index: number) => {
-	activeCurrent.value = index;
+const findingWeather = async (item: any) => {
+	activeCurrent.value = item.state;
 	await findWeather(item.name);
 };
 </script>
